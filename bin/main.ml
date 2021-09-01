f4eecf08abfbb8dafa2c6937923b6564d4376a2b
Magnus Skjegstad <magnus@skjegstad.com>
2021-09-01 15:51:46 +0200

    Add internal report options
    Adds options for
    - show/hide eng time
    - show/hide intermediate time calculation for aggregated reports
    - include only a specific set of KR IDs
    - ignore some subsections (by default OKR updates)

--- bin/main.ml
+++ bin/main.ml
@@ -17,37 +17,92 @@
 
 open Okra
 
-let () =
-  let md = Omd.of_channel stdin in
-  let okrs = Okra.process md in
+(** [report_team_md okrs] outputs a team report to stdout. 
 
-  (*Hashtbl.iter (fun _ v ->
-    print_okrs v) store.okr_ht*)
+[ignore_sections] ignores a list of subsections in the report. By default this
+is set to "OKR updates".
+
+[include_krs] only includes this list of KR IDs. Note that this will ignore
+empty KR IDs or KRs marked as "NEW KR" unless specified in the list. If the
+list is empty, all KRs are returned.
+
+When [show_time_calc] is set, an extra line will be added to the output each
+time the same entry is included in the report with a sum at the end. This is
+useful for showing the intermediate steps when aggreating multiple reports that
+contain the same KR.
+
+Example output with [show_time_calc] enabled:
+
+- This is a KR (KR ID)
+  + @engineer1 (1 day), @engineer2 (1 day)
+  + @engineer1 (1 day)
+  = @engineer1 (2 days), @engineer2 (1 day)
+  - Work item
+  ...
+
+[show_time] shows the time entries
+[show_engineers] shows the list of engineers
+
+*)
+let report_team_md ?ignore_sections:(ignore_sections=["OKR updates"])
+		   ?include_krs:(include_krs=[])
+		   ?show_time:(show_time=true)
+  		   ?show_time_calc:(show_time_calc=true)
+                   ?show_engineers:(show_engineers=true)
+		   okrs =
   let v = List.map Okra.of_weekly (List.of_seq (Hashtbl.to_seq_values okrs)) in
+  let uppercase_include_krs = (List.map String.uppercase_ascii include_krs) in
+  let uppercase_ignore_sections = (List.map String.uppercase_ascii ignore_sections) in
   let c_project = ref "" in
   let c_objective = ref "" in
   let c_kr_id = ref "" in
   let c_kr_title = ref "" in
   List.iter
     (fun e ->
-      if e.project <> !c_project then (
-        Printf.printf "\n# %s\n" e.project;
-        c_project := e.project)
-      else ();
-      if e.objective <> !c_objective then (
-        Printf.printf "\n## %s\n" e.objective;
-        c_objective := e.objective)
-      else ();
-      if e.kr_id <> !c_kr_id || e.kr_title <> !c_kr_title then (
-        Printf.printf "\n- %s (%s)\n" e.kr_title e.kr_id;
-        c_kr_title := e.kr_title;
-        c_kr_id := e.kr_id)
-      else ();
-      List.iter (fun s -> Printf.printf "    - + %s" s) e.time_entries;
-      Printf.printf "    - = ";
-      Hashtbl.iter
-        (fun s v -> Printf.printf "@%s (%.2f days) " s v)
-        e.time_per_engineer;
-      Printf.printf "\n";
-      List.iter (fun s -> Printf.printf "    - %s" s) e.work)
+      (* only proceed if include_krs is empty or has a match, and ignore_sections is empty or without match *)
+      if ((List.length include_krs == 0 || List.mem e.kr_id uppercase_include_krs)) &&
+	 ((List.length ignore_sections == 0 || not (List.mem (String.uppercase_ascii e.project) uppercase_ignore_sections))) then (
+	      if e.project <> !c_project then (
+		Printf.printf "\n# %s\n" e.project;
+		c_project := e.project)
+	      else ();
+	      if e.objective <> !c_objective then (
+		Printf.printf "\n## %s\n" e.objective;
+		c_objective := e.objective)
+	      else ();
+	      if e.kr_id <> !c_kr_id || e.kr_title <> !c_kr_title then (
+		Printf.printf "\n- %s (%s)\n" e.kr_title e.kr_id;
+		c_kr_title := e.kr_title;
+		c_kr_id := e.kr_id)
+	      else ();
+	      if show_engineers then (
+		      if show_time then (
+			      if show_time_calc then ( (* show time calc + engineers *)
+				      List.iter (fun s -> Printf.printf "    - + %s" s) e.time_entries;
+				      Printf.printf "    - = ";
+				      Hashtbl.iter
+					(fun s v -> Printf.printf "@%s (%.2f days) " s v)
+					e.time_per_engineer;
+				      Printf.printf "\n")
+			      else ( (* show total time for each engineer *)
+				      Printf.printf "    - ";
+				      Hashtbl.iter
+					(fun s v -> Printf.printf "@%s (%.2f days) " s v)
+					e.time_per_engineer;
+				      Printf.printf "\n"))
+		      else ( (* only show engineers, no time *)
+			      Printf.printf "    - ";
+			      Hashtbl.iter
+				(fun s _ -> Printf.printf "@%s " s)
+				e.time_per_engineer;
+			      Printf.printf "\n"))
+	      else (); (* don't show time or engineers *)
+	      List.iter (fun s -> Printf.printf "    - %s" s) e.work)
+      else () (* skip this KR *)
+    )
     (List.sort Okra.compare v)
+
+let () =
+  let md = Omd.of_channel stdin in
+  let okrs = Okra.process md in
+  report_team_md okrs
